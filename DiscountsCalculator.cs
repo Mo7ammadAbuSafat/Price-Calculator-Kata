df0ef38513573764b57f2fe9129bd6f647499841
Mohammad AbuSafat <mo7ammad.abusafat@gmail.com>
2022-12-21 08:47:33 +0200

    EXPENSES save changes for switch to another branch

---
+++ DiscountsCalculator.cs
@@ -0,0 +1,67 @@
+﻿namespace Price_Calculator_Kata
+{
+    public class DiscountsCalculator
+    {
+
+        public StoreRules storeRules;
+        public Product product;
+
+        public DiscountsCalculator(StoreRules storeRules, Product product)
+        {
+            this.storeRules = storeRules;
+            this.product = product;
+        }
+
+        public double CalculateSpecialDiscount()
+        {
+            if (storeRules.getSpecialDiscount() == null) return 0;
+            if (product.UPC == storeRules.getSpecialDiscount().UPC)
+            {
+                return Math.Round(storeRules.getSpecialDiscount().Percentage * product.Price, 2);
+            }
+            else return 0;
+        }
+
+        public double CalculateUniversalDiscount()
+        {
+            if (storeRules.getUniversalDiscount() == null)
+                return 0;
+            return Math.Round(storeRules.getUniversalDiscount().Percentage * product.Price, 2);
+        }
+
+        public double? CalculatePreTaxDiscount()
+        {
+            double PreTaxDiscount = 0;
+            if (storeRules.getUniversalDiscount()?.Type == DiscountType.PRE_TAX)
+            {
+                PreTaxDiscount = Math.Round(PreTaxDiscount + CalculateUniversalDiscount(), 2);
+            }
+            if (storeRules.getSpecialDiscount()?.Type == DiscountType.PRE_TAX)
+            {
+                PreTaxDiscount = Math.Round(PreTaxDiscount + CalculateSpecialDiscount(), 2);
+            }
+
+            return PreTaxDiscount == 0 ? null : PreTaxDiscount;
+        }
+
+        public double? CalculatePostTaxDiscount()
+        {
+            double PostTaxDiscount = 0;
+            if (storeRules.getUniversalDiscount()?.Type == DiscountType.POST_TAX)
+            {
+                PostTaxDiscount = Math.Round(PostTaxDiscount + CalculateUniversalDiscount(), 2);
+            }
+            if (storeRules.getSpecialDiscount()?.Type == DiscountType.POST_TAX)
+            {
+                PostTaxDiscount = Math.Round(PostTaxDiscount + CalculateSpecialDiscount(), 2);
+            }
+
+            return PostTaxDiscount == 0 ? null : PostTaxDiscount;
+        }
+
+        public double CalculateTotalDiscount()
+        {
+            return Math.Round(CalculateSpecialDiscount() + CalculateUniversalDiscount(), 2);
+        }
+    }
+}
---
+++ StoreRules.cs
@@ -0,0 +1,76 @@
+﻿using System.Diagnostics;
+
+namespace Price_Calculator_Kata
+{
+    public class StoreRules
+    {
+        private double _taxPercentage;
+        public double TaxPercentage
+        {
+            get => _taxPercentage;
+            set
+            {
+                CheckPercentageValidation(value, "Tax");
+                _taxPercentage = Math.Round(value, 2);
+            }
+        }
+
+        private UniversalDiscount? _universalDiscount;
+
+        public UniversalDiscount? getUniversalDiscount()
+        {
+            return _universalDiscount;
+        }
+
+        public void setUniversalDiscount(UniversalDiscount universalDiscount)
+        {
+            CheckPercentageValidation(universalDiscount.Percentage, "Universal discount");
+            _universalDiscount = universalDiscount;
+
+        }
+
+
+        private SpecialDiscount? _specialDiscount;
+
+        public SpecialDiscount? getSpecialDiscount()
+        {
+            return _specialDiscount;
+        }
+
+        public void setSpecialDiscount(SpecialDiscount specialDiscount)
+        {
+            CheckPercentageValidation(specialDiscount.Percentage, "Special discount");
+            _specialDiscount = specialDiscount;
+        }
+
+        public StoreRules()
+        {
+        }
+        public StoreRules(double taxPercentage) 
+        {
+            TaxPercentage = taxPercentage;
+        }
+
+        public StoreRules(double taxPercentage, UniversalDiscount universalDiscount)
+        {
+            TaxPercentage = taxPercentage;
+            setUniversalDiscount(universalDiscount);
+        }
+
+        public StoreRules(double taxPercentage, UniversalDiscount universalDiscount, SpecialDiscount specialDiscount)
+        {
+            TaxPercentage = taxPercentage;
+            setUniversalDiscount(universalDiscount);
+            setSpecialDiscount(specialDiscount);
+        }
+
+        public static void CheckPercentageValidation(double percentage, string percentageName)
+        {
+            if (percentage < 0 || percentage > 1)
+            {
+                throw new ArgumentException($"{percentageName} percentage must be between 0 and 1.");
+            }
+        }
+
+    }
+}
